88d972d4e780589aa160315e12c7e12b72849747
Yong-Park <yong.park123456@gmail.com>
2023-10-08 20:54:05 -0400

    changes maded

--- bruteforce_naive.cpp
+++ bruteforce_naive.cpp
@@ -11,14 +11,16 @@
 void des_encrypt(const unsigned char *key, const char *plaintext, char *ciphertext) {
     DES_key_schedule keysched;
     DES_cblock des_key;
-    DES_set_key_unchecked(key, &keysched); // Utiliza la misma clave aquí
+    DES_string_to_key((const char *)key, &des_key);
+    DES_set_key_unchecked(&des_key, &keysched);
     DES_ecb_encrypt((const_DES_cblock *)plaintext, (DES_cblock *)ciphertext, &keysched, DES_ENCRYPT);
 }
 
 void des_decrypt(const unsigned char *key, const char *ciphertext, char *plaintext) {
     DES_key_schedule keysched;
     DES_cblock des_key;
-    DES_set_key_unchecked(key, &keysched); // Utiliza la misma clave aquí
+    DES_string_to_key((const char *)key, &des_key);
+    DES_set_key_unchecked(&des_key, &keysched);
     DES_ecb_encrypt((const_DES_cblock *)ciphertext, (DES_cblock *)plaintext, &keysched, DES_DECRYPT);
 }
 
@@ -31,22 +33,28 @@ void increment_key(unsigned char key[], int key_length) {
     }
 }
 
-void bruteforce(const char *ciphertext, const char *known_substring, int key_length, uint64_t max_tries, int id, int num_procs, const unsigned char *key) { // Pasa la misma clave aquí
+void bruteforce(const char *ciphertext, const char *known_substring, int key_length, uint64_t max_tries, int id, int num_procs, bool &found) {
+    unsigned char key[key_length] = {0};
+    char encrypted_known[64];
     char decryptedtext[64];
 
     uint64_t range_per_node = max_tries / num_procs;
     uint64_t mylower = range_per_node * id;
     uint64_t myupper = range_per_node * (id + 1);
 
+    // Cifra la cadena conocida una vez para comparar con el texto cifrado
+    des_encrypt(key, known_substring, encrypted_known);
+
     for (uint64_t i = mylower; i < myupper; i++) {
-        des_decrypt(key, ciphertext, decryptedtext);
-        if (strstr(decryptedtext, known_substring) != nullptr) {
+        des_encrypt(key, known_substring, encrypted_known); // Cifra la cadena conocida con la clave actual
+        if (strcmp(encrypted_known, ciphertext) == 0) { // Compara el resultado cifrado con el texto cifrado proporcionado
+            found = true; // Establece found en true si encuentra la clave
             std::cout << "Llave encontrada por proceso " << id << ": ";
             for (int j = 0; j < key_length; j++) {
                 std::cout << (int)key[j] << " ";
             }
             std::cout << std::endl;
-            std::cout << "Texto descifrado: " << decryptedtext << std::endl;
+            std::cout << "Texto descifrado: " << known_substring << std::endl;
 
             // Enviar un mensaje a los demás procesos para detener la búsqueda
             for (int proc = 0; proc < num_procs; proc++) {
@@ -68,6 +76,8 @@ void bruteforce(const char *ciphertext, const char *known_substring, int key_len
     }
 }
 
+
+
 int main(int argc, char *argv[]) {
     MPI_Init(&argc, &argv);
 
@@ -87,6 +97,7 @@ int main(int argc, char *argv[]) {
     std::string llave_privada_str = argv[2];
     const char *known_substring = "es una prueba de";
     char ciphertext[64];
+    unsigned char key[8] = {0}; // Cambiado a 8 bytes para manejar números largos
 
     // Leer el texto desde el archivo (solo el proceso 0 lo hace)
     if (id == 0) {
@@ -102,7 +113,7 @@ int main(int argc, char *argv[]) {
 
         // Convertir la llave privada a un número largo
         uint64_t llave_privada = std::stoull(llave_privada_str);
-        unsigned char key[8] = {0}; // Cambiado a 8 bytes para manejar números largos
+        
         for (int i = 0; i < 8; i++) {
             key[i] = (llave_privada >> (i * 8)) & 0xFF;
         }
@@ -113,20 +124,20 @@ int main(int argc, char *argv[]) {
         // Mostrar el texto cifrado
         std::cout << "Texto cifrado: " << ciphertext << std::endl;
 
-        // Broadcast la clave a todos los demás procesos
-        MPI_Bcast(key, 8, MPI_CHAR, 0, MPI_COMM_WORLD);
-    } else {
-        unsigned char key[8]; // Recibir la clave de otros procesos
-        MPI_Bcast(key, 8, MPI_CHAR, 0, MPI_COMM_WORLD);
     }
+    // Broadcast la clave a todos los demás procesos
+    MPI_Bcast(key, 8, MPI_CHAR, 0, MPI_COMM_WORLD);
 
+    bool found = false;
     // Realizar el ataque de fuerza bruta para encontrar la clave
     clock_t start_time = clock();
-    bruteforce(ciphertext, known_substring, 8, 1000000000, id, num_procs, key); // Cambiado a 8 bytes para manejar números largos
+    bruteforce(ciphertext, known_substring, 8, 1000000000, id, num_procs,found); // Cambiado a 8 bytes para manejar números largos
     clock_t end_time = clock();
     double time_taken = double(end_time - start_time) / CLOCKS_PER_SEC;
 
-    if (id == 0) {
+    MPI_Barrier(MPI_COMM_WORLD);
+
+    if (found && id == 0) {
         std::cout << "\nTiempo tomado para encontrar la llave: " << time_taken << " segundos" << std::endl;
     }
 
